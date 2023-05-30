98c51a47aa2b53b2799baea9562f8cab5e2c9cac
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-05-30 17:13:11 +0200

    add in page with template literal

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 console.log('JS OK')
 
 
+// recupero elemento dal dom
+
+const containerElement = document.getElementById('my-container')
+
+
 // ciclo che stampa da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('fizz-buzz')
-    }
 
+    let stringa = ''
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+        stringa = 'fizzbuzz'
+        containerElement.innerHTML += `<div>${stringa}</div>`;
+    }
+    
     else if (i % 3 === 0) {
         console.log('fizz')
+        stringa = 'fizz'
+        containerElement.innerHTML += `<div>${stringa}</div>`;
     }
     
     else if (i % 5 === 0) {
         console.log('buzz')
+        stringa = 'buzz'
+        containerElement.innerHTML += `<div>${stringa}</div>`;
     }
     else {
         console.log(i)
+        containerElement.innerHTML += `<div>${i}</div>`;
     }
     
 }
+
