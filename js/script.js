185e7f54e2195481798a5324223cecf0e4bd1fb9
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-05-31 19:57:05 +0200

    fix js template literal

--- js/script.js
+++ js/script.js
@@ -9,27 +9,16 @@ const containerElement = document.getElementById('my-container')
 // ciclo che stampa da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
-    let stringa = ''
+    let stringa = i 
+    
+    if (i % 3 === 0 && i % 5 === 0) stringa = 'fizzbuzz'
 
-    if (i % 3 === 0 && i % 5 === 0) {
-        stringa = 'fizzbuzz'
-        containerElement.innerHTML += `<div class="myCard fizzbuzz-square text-center">${stringa}</div>`;
-    }
+    else if (i % 3 === 0) stringa = 'fizz'
     
-    else if (i % 3 === 0) {     
-        stringa = 'fizz'
-        containerElement.innerHTML += `<div class="myCard fizz-square text-center ">${stringa}</div>`;
-    }
-    
-    else if (i % 5 === 0) {
-        stringa = 'buzz'
-        containerElement.innerHTML += `<div class="myCard buzz-square text-center ">${stringa}</div>`;
-    }
-    else {
-        stringa = i;
-        containerElement.innerHTML += `<div class="myCard number-square text-center">${stringa}</div>`;
-    }
+    else if (i % 5 === 0) stringa = 'buzz'
 
     console.log(stringa)
-}
+    containerElement.innerHTML += `<div class="${stringa} myCard text-center">${stringa}</div>`;
+    }
 
+    
